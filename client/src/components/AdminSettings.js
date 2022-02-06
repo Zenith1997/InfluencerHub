7817ef4cd562ca69471ccc995728fead29722107
Kumuthu Dulnath <kumuthudulnath@gmail.com>
2022-02-06 14:31:05 +0530

    added admin registration system

--- client/src/components/AdminSettings.js
+++ client/src/components/AdminSettings.js
@@ -1,65 +1,134 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
 import Menu from './Menu'
 import {Accordion,Container,Row,Col,Form,Button} from 'react-bootstrap'
+import bodyParser from 'body-parser';
 
-class AdminSettings extends Component{
-    render(){
-        return(
-            <div>
-                <Menu/>
-                <Accordion defaultActiveKey="0" style={{padding:"10px 40px 0px 40px"}}>
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header className="text-decoration-none text-dark">Register new Admin</Accordion.Header>
-                        <Accordion.Body>
-                            <Container>
-                                <Form>
-                                    <Row>
-                                        <Col>
-                                            <Form.Group className="mb-3" controlId="formFirstName">
-                                                <Form.Control type="text" placeholder="First Name" />
-                                            </Form.Group>
-                                        </Col>
-                                        <Col>
-                                            <Form.Group className="mb-3" controlId="formLastName">
-                                                <Form.Control type="text" placeholder="Last Name" />
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <Form.Group className="mb-3" controlId="formEmail">
-                                                <Form.Control type="email" placeholder="Email" />
-                                            </Form.Group>
-                                        </Col>
-                                        <Col>
-                                            <Form.Group className="mb-3" controlId="formContactNo">
-                                                <Form.Control type="text" placeholder="Contact Number" />
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <Form.Group className="mb-3" controlId="formPassword">
-                                                <Form.Control type="password" placeholder="Password" />
-                                            </Form.Group>
-                                        </Col>
-                                        <Col>
-                                            <Form.Group className="mb-3" controlId="formPasswordRep">
-                                                <Form.Control type="password" placeholder="Repeat Password" />
-                                            </Form.Group>
-                                        </Col>
-                                        <Button variant="primary" type="submit">
-                                            Register
-                                        </Button>
-                                    </Row>
-                                </Form>
-                            </Container>
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion> 
-            </div>       
-        )
+function AdminSettings(){
+
+    const [fname, setFName] = useState('')
+    const [lname,setLName] = useState('')
+    const [email, setEmail] = useState('')
+    const [contactNo,setContactNo] = useState('');
+    const [password, setPassword] = useState('')
+    const [passwordRep,setPasswordRep] = useState('');
+    const navigate = useNavigate();
+
+    async function registerUser(event) {
+        event.preventDefault()
+
+        
+        if(password === passwordRep){
+            const response = await fetch('http://localhost:5000/api/useraccounts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    fname,
+                    lname,
+                    email,
+                    contactNo,
+                    password
+                }),
+            })
+
+            const data = await response.json()
+
+            if (data.status === 'ok') {
+                console.log('data submitted')
+                navigate('../dashboard');
+                alert('Sucessfully registered New Admin')   
+            }
+        }else{
+            alert('Passwords do not match!!!');
+        }
+
+        
     }
+
+
+
+    return(
+        <div>
+            <Menu/>
+            <Accordion defaultActiveKey="0" style={{padding:"10px 40px 0px 40px"}}>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header className="text-decoration-none text-dark">Register new Admin</Accordion.Header>
+                    <Accordion.Body>
+                        <Container>
+                            <Form onSubmit={registerUser}>
+                                <Row>
+                                    <Col>
+                                        <Form.Group className="mb-3" controlId="formFirstName">
+                                            <Form.Control
+                                             value={fname}
+                                             onChange={(e)=>setFName(e.target.value)}
+                                             type="text" 
+                                             placeholder="First Name" />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group className="mb-3" controlId="formLastName">
+                                            <Form.Control
+                                             value={lname}
+                                             onChange={(e)=>setLName(e.target.value)}
+                                             type="text" 
+                                             placeholder="Last Name" />
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Form.Group className="mb-3" controlId="formEmail">
+                                            <Form.Control 
+                                            value={email}
+                                            onChange={(e)=>setEmail(e.target.value)}
+                                            type="email" 
+                                            placeholder="Email" />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group className="mb-3" controlId="formContactNo">
+                                            <Form.Control
+                                            value={contactNo}
+                                            onChange={(e)=>setContactNo(e.target.value)} 
+                                            type="text" 
+                                            placeholder="Contact Number" />
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Form.Group className="mb-3" controlId="formPassword">
+                                            <Form.Control 
+                                            value={password}
+                                            onChange={(e)=>setPassword(e.target.value)}
+                                            type="password" 
+                                            placeholder="Password" />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group className="mb-3" controlId="formPasswordRep">
+                                            <Form.Control
+                                            value={passwordRep}
+                                            onChange={(e)=>setPasswordRep(e.target.value)} 
+                                            type="password" 
+                                            placeholder="Repeat Password" />
+                                        </Form.Group>
+                                    </Col>
+                                    <Button variant="primary" type="submit">
+                                        Register
+                                    </Button>
+                                </Row>
+                            </Form>
+                        </Container>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion> 
+        </div>       
+    )
 }
 
 export default AdminSettings;
--- routes/api/useraccounts.js
+++ routes/api/useraccounts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 const UserAccount = require('../../models/UserAccount');
 
 router.get('/',(req,res) => {
@@ -8,5 +8,25 @@ router.get('/',(req,res) => {
         .sort({date:-1})
         .then(items => res.json(items))
 });
+
+router.post('/', async (req, res) => {
+	console.log(req.body)
+	try {
+		const newPassword = await bcrypt.hash(req.body.password, 10);
+        console.log(newPassword);
+		await UserAccount.create({
+			firstName: req.body.fname,
+            lastName: req.body.lname,
+			email: req.body.email,
+            phoneNo: req.body.contactNo,
+            type:'admin',
+            isActive:'true',
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' });
+	}
+})
 
 module.exports = router;
